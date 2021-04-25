4e045fe4dc04eedf27103908f48a71ba8a0b01f8
Chris Hanma <hongchris96@hotmail.com>
2021-04-25 15:10:54 -0700

    Add webpack config and package json

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: [".js", ".jsx", "*"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
