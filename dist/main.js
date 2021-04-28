394fbca5cd0c92e73f36fc1102d64085c365b451
Chris Hanma <hongchris96@hotmail.com>
2021-04-28 12:20:27 -0700

    Add Title and catchphrase display

--- dist/main.js
+++ dist/main.js
@@ -855,16 +855,34 @@ const GameView = __webpack_require__(/*! ./classes/game_view */ "./src/classes/g
 const Goose = __webpack_require__(/*! ./classes/goose */ "./src/classes/goose.js");
 
 document.addEventListener("DOMContentLoaded", (e) => {
+  
+  const title = document.querySelector('.title-sign');
+  const phrases = document.querySelectorAll('.catchphrases');
+  const menu = document.querySelector('.pre-menu');
+  
   const kanvas = document.getElementById("game-canvas");
   const cntx = kanvas.getContext("2d");
 
-  // testing
+  title.addEventListener('click', () => {
+    title.classList.add('hidden');
+    phrases.forEach((phrase, idx) => {
+      setTimeout(() => {
+        phrase.classList.remove('hidden');
+        phrase.classList.add('fade-in');
+      }, idx * idx * 500 + idx * 3500 + 1000);
+      setTimeout(() => {phrase.classList.add('hidden');}, idx * idx * 500 + idx * 4500 + 4000);
+    })
+    
+    setTimeout(() => {
+      kanvas.classList.remove('hidden');
+      kanvas.classList.add('fade-in');
+      const zaGame = new GameView(cntx);
+      zaGame.start();
+    }, 22500)
+  });
 
-  // -------
-  
-  const zaGame = new GameView(cntx);
-  zaGame.start();
 });
+
 })();
 
 /******/ })()
--- src/index.js
+++ src/index.js
@@ -2,13 +2,30 @@ const GameView = require('./classes/game_view');
 const Goose = require('./classes/goose');
 
 document.addEventListener("DOMContentLoaded", (e) => {
+  
+  const title = document.querySelector('.title-sign');
+  const phrases = document.querySelectorAll('.catchphrases');
+  const menu = document.querySelector('.pre-menu');
+  
   const kanvas = document.getElementById("game-canvas");
   const cntx = kanvas.getContext("2d");
 
-  // testing
+  title.addEventListener('click', () => {
+    title.classList.add('hidden');
+    phrases.forEach((phrase, idx) => {
+      setTimeout(() => {
+        phrase.classList.remove('hidden');
+        phrase.classList.add('fade-in');
+      }, idx * idx * 500 + idx * 3500 + 1000);
+      setTimeout(() => {phrase.classList.add('hidden');}, idx * idx * 500 + idx * 4500 + 4000);
+    })
+    
+    setTimeout(() => {
+      kanvas.classList.remove('hidden');
+      kanvas.classList.add('fade-in');
+      const zaGame = new GameView(cntx);
+      zaGame.start();
+    }, 22500)
+  });
 
-  // -------
-  
-  const zaGame = new GameView(cntx);
-  zaGame.start();
 });
